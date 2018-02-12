4af930e9c9993aff2c4d4fb1a47700861ca0d9cb
Jcardif <josh.cardif14@outlook.com>
2018-02-11 17:16:33 -0800

    Initialised solution

---
+++ SevenSegmentDissplay/SevenSegmentDissplay.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		SevenSegmentDissplay.ino
+ Created:	2/11/2018 5:11:27 PM
+ Author:	Josh
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
